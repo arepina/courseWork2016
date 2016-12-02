68e29bef4194ebc48eb1e8cb2c12bb795f561f1e
anastasia <anastasiya.repina2012@yandex.ru>
2016-12-02 22:30:51 +0300

    New version of aspects db

--- aspects/Aspects.py
+++ aspects/Aspects.py
@@ -1,19 +1,13 @@
 import json
 import os
-
-import sys
-
-# from nltk.text import TextCollection
 import requests
 
 from sklearn.model_selection import train_test_split
-
-from aspects.AspectsDB import AspectsDB
 from sklearn import svm
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from aspects.IdealAspectsDB import IdealAspectsDB
-
+from aspects.AspectsDB import AspectsDB
 
 class Aspects:
     api_key = "43de6ee952010c5e0870b999f2a1949183456c73"
@@ -25,66 +19,42 @@ class Aspects:
     def __init__(self):
         self.url_syntatic_parsing += self.api_key
         self.url_pos += self.api_key
-        # row = aspect_db.cursor_merged.execute('SELECT * FROM Reviews').fetchone()
-        # texts_arr = []
-        # while row is not None:  # iterate through all reviews
-        #     texts_arr.append(str(row[0]).lower())
-        #     row = aspect_db.cursor_merged.fetchone()
-        # self.texts = TextCollection(texts_arr)  # add all values to text collection
 
-    def aspects_find(self):
+    def process(self):
         row_aspect = aspect_db.cursor_reviews.execute('SELECT * FROM Review').fetchone()
         count = 0
         while row_aspect is not None:  # iterate through all reviews
             print(count)
             count += 1
-            if count > 3063:
+            if count > 14347:
                 article = str(row_aspect[2])
                 adv = str(row_aspect[3])
                 dis = str(row_aspect[4])
                 com = str(row_aspect[5])
                 if len(adv) != 0:
                     adv_parsed = self.syntatic_parsing(adv)
-                    while adv_parsed is None:
-                        adv_parsed = self.syntatic_parsing(adv)
-                    list_adv_aspects = self.aspects(adv_parsed)  # load aspects for advantage
+                    list_adv_aspects = self.aspects_find(adv_parsed)  # load aspects for advantage
                 else:
                     list_adv_aspects = []
 
                 if len(dis) != 0:
                     dis_parsed = self.syntatic_parsing(dis)
-                    while dis_parsed is None:
-                        dis_parsed = self.syntatic_parsing(dis)
-                    list_dis_aspects = self.aspects(dis_parsed)  # load aspects for disadvantage
+                    list_dis_aspects = self.aspects_find(dis_parsed)  # load aspects for disadvantage
                 else:
                     list_dis_aspects = []
 
                 if len(com) != 0:
                     com_parsed = self.syntatic_parsing(com)
-                    while com_parsed is None:
-                        com_parsed = self.syntatic_parsing(com)
-                    list_com_aspects = self.aspects(com_parsed)  # load aspects for comment
+                    list_com_aspects = self.aspects_find(com_parsed)  # load aspects for comment
                 else:
                     list_com_aspects = []
-
-                # calculate td-idf value for each aspect
-                tdidf_adv = self.td_idf_calculate(list_adv_aspects, adv)
-                tdidf_dis = self.td_idf_calculate(list_dis_aspects, dis)
-                tdidf_com = self.td_idf_calculate(list_com_aspects, com)
                 # join the results
-                str_adv_aspects = ';'.join(tdidf_adv)
-                str_dis_aspects = ';'.join(tdidf_dis)
-                str_com_aspects = ';'.join(tdidf_com)
+                str_adv_aspects = ';'.join(list_adv_aspects)
+                str_dis_aspects = ';'.join(list_dis_aspects)
+                str_com_aspects = ';'.join(list_com_aspects)
                 # add found information to DB
                 aspect_db.add_review(article, str_adv_aspects, str_dis_aspects, str_com_aspects)
             row_aspect = aspect_db.cursor_reviews.fetchone()
-
-    def td_idf_calculate(self, aspects_list, review_part):
-        result = []
-        for aspect_item in aspects_list:
-            value = self.texts.tf_idf(aspect_item.lower(), review_part.lower())
-            result.append(aspect_item + "{" + str(value) + "}")
-        return result
 
     @staticmethod
     def syntatic_parsing(review):  # detects syntactic structure for each sentence of a given text
@@ -96,11 +66,12 @@ class Aspects:
                 r = requests.post(aspect.url_syntatic_parsing, data=payload, headers=headers)
             return r.content.decode('utf8')
         except Exception:
+            import sys
             type, value, traceback = sys.exc_info()  # (<class 'requests.exceptions.ConnectionError'>, ConnectionError(ProtocolError('Connection aborted.', TimeoutError(10060, 'Попытка установить соединение была безуспешной, т.к. от другого компьютера за требуемое время не получен нужный отклик, или было разорвано уже установленное соединение из-за неверного отклика уже подключенного компьютера', None, 10060, None)),), <traceback object at 0x0667AB48>)
             print('Error opening %s: %s' % (value.filename, value.strerror))
             return None
 
-    def aspects(self, part):  # find aspects in each review part
+    def aspects_find(self, part):  # find aspects in each review part
         list_aspects = []  # list with aspects
         print(part)
         data = json.loads(part)
@@ -108,89 +79,59 @@ class Aspects:
         pos_arr = self.parse_pos(self.tag_part_of_speech(data['text']))
         for item in items:  # iterate through words/word pairs in concrete review
             if 'parent' in item['value']:  # word pair
-                list_aspects = self.word_pair(data, item, items, list_aspects,
-                                              pos_arr)  # look for aspect noun(parent) + word
+                # look for aspect noun(parent) + word
+                list_aspects = self.word_pair(data, item, list_aspects, pos_arr)
             else:  # one word
-                found = False
-                for aspect_item in list_aspects:
-                    if data['text'][item['start']:item[
-                        'end']] in aspect_item:  # we have already found a better variant for these word
-                        found = True
-                        break
-                if not found:
-                    list_aspects = self.one_word(item, data, list_aspects, pos_arr)  # look for aspect noun(word)
+                # look for aspect noun(word)
+                list_aspects = self.one_word(data, item, list_aspects, pos_arr)
         return list_aspects
 
-    @staticmethod
-    def one_word(item, data, list_aspects, pos_arr):
+    def one_word(self, data, item, list_aspects, pos_arr):
         start = item['start']
         end = item['end']
         word = data['text'][start:end]
         if pos_arr[str(word)] == 'S':  # our word is noun
-            list_aspects.append(word.lower())  # add an aspect noun(our word)
+            list_aspects.append(self.replacer(word))  # add an aspect noun(our word)
         return list_aspects
 
-    def word_pair(self, data, item, items, list_aspects, pos_arr):
-        try:
-            word = data['text'][item['start']:item['end']]
-            parent = data['text'][item['value']['parent']['start']:item['value']['parent']['end']]
-            pos_parent = pos_arr[str(parent)]
-            pos_word = pos_arr[str(word)]
-            if (pos_parent == 'S' and pos_word != 'PUNCT' and pos_word != 'CONJ' and pos_word != 'PR') \
-                    or (pos_word == 'S' and pos_parent != 'PUNCT' and pos_parent != 'CONJ'):
-                # don't need punctuations, conjunctions, pretexts as main words
-                # find pairs: S(parent) + smf or S(word) + smf
-                start_par = item['value']['parent']['start']
-                start_word = item['start']
-                if pos_parent == 'PR' or pos_parent == 'V':  # try to find PART for PR or for V (не для *, не доделал *)
-                    list_aspects = self.part_find(items, data, item['value']['parent'], parent, word, list_aspects,
-                                                  pos_arr, start_par, start_word)
-                else:
-                    if start_par < start_word:  # the word order is important in if-idf calculation
-                        list_aspects.append(
-                            parent.lower() + " " + word.lower())  # add an aspect noun(parent) + our word
-                    else:
-                        list_aspects.append(
-                            word.lower() + " " + parent.lower())  # add an aspect noun(parent) + our word
-        except:
-            pass
+    def word_pair(self, data, item, list_aspects, pos_arr):
+        word = data['text'][item['start']:item['end']]
+        parent = data['text'][item['value']['parent']['start']:item['value']['parent']['end']]
+        pos_parent = pos_arr[str(parent)]
+        pos_word = pos_arr[str(word)]
+        if pos_parent == 'S' and pos_word != 'PUNCT' and pos_word != 'CONJ' and pos_word != 'PR':
+            # don't need punctuations, conjunctions, pretexts as words
+            # find pairs: S(parent) + smf
+            start_par = item['value']['parent']['start']
+            start_word = item['start']
+            if start_par < start_word:  # the word order is important in if-idf calculation
+                # add an aspect noun(parent) + our word
+                list_aspects.append(self.replacer(parent) + " " + self.replacer(word))
+            else:
+                # add an aspect noun(parent) + our word
+                list_aspects.append(self.replacer(word) + " " + self.replacer(parent))
+        elif pos_word == 'S':
+            list_aspects.append(self.replacer(word))  # add an aspect noun(our word)
         return list_aspects
 
     @staticmethod
-    def part_find(items, data, parent, parent_value, word, list_aspects, pos_arr, start_par, start_word):
-        for item in items:  # look through all words in review part
-            if 'parent' in item['value']:
-                word_value_extra = data['text'][item['start']:item['end']]
-                parent_value_extra = data['text'][item['value']['parent']['start']:item['value']['parent']['end']]
-                if item['value']['parent']['start'] == parent['start'] \
-                        and item['value']['parent']['end'] == parent['end'] \
-                        and parent_value == parent_value_extra:  # the found word and word from parameters are the same
-                    pos_word_extra = pos_arr[str(word_value_extra)]
-                    if pos_word_extra == 'PART' and item['value']['type'] != '2-компл':
-                        start_extra_word = item['start']
-                        sum = ""
-                        if start_word < start_par and start_word < start_extra_word:  # the word order is important in if-idf calculation
-                            if start_extra_word < start_par:
-                                sum += word.lower() + " " + word_value_extra.lower() + " " + parent_value.lower()  # wep
-                            else:
-                                sum += word.lower() + " " + parent_value.lower() + " " + word_value_extra.lower()  # wpe
-                        elif start_par < start_extra_word and start_par < start_word:
-                            if start_word < start_extra_word:
-                                sum += parent_value.lower() + " " + word.lower() + " " + word_value_extra.lower()  # pwe
-                            else:
-                                sum += parent_value.lower() + " " + word_value_extra.lower() + " " + word.lower()  # pew
-                        elif start_extra_word < start_word and start_extra_word < start_par:
-                            if start_par < start_word:
-                                sum += word_value_extra.lower() + " " + parent_value.lower() + " " + word.lower()  # epw
-                            else:
-                                sum += word_value_extra.lower() + " " + word.lower() + " " + parent_value.lower()  # ewp
-                        list_aspects.append(sum)  # add an aspect part + noun(parent) + our word
-                        return list_aspects
-        if start_par < start_word:  # the word order is important in if-idf calculation
-            list_aspects.append(parent_value.lower() + " " + word.lower())  # add an aspect noun(parent) + our word
-        else:
-            list_aspects.append(word.lower() + " " + parent_value.lower())  # add an aspect noun(parent) + our word
-        return list_aspects
+    def replacer(item):
+        item = item.replace(",", "")
+        item = item.replace(".", "")
+        item = item.replace("•", "")
+        item = item.replace(";", "")
+        item = item.replace("!", "")
+        item = item.replace("?", "")
+        item = item.replace(")", "")
+        item = item.replace("(", "")
+        item = item.replace("™", "")
+        item = item.replace("®", "")
+        item = item.replace("*", "")
+        item = item.replace("\"", "")
+        item = item.replace("—", "")
+        item = item.replace("~", "")
+        item = item.replace("'", "")
+        return item.lower()
 
     @staticmethod
     def parse_pos(pos_sentence):
@@ -365,7 +306,8 @@ class Synonyms:
 
 
 aspect_db = AspectsDB()  # aspects data base
-aspect = Aspects()  # find aspects with the help of ISP RAS API
+aspect = Aspects()
+#aspect.process()  # find aspects with the help of ISP RAS API
 one_class_svm = OneClassSVM()
 data = one_class_svm.get_data()  # get only aspects from data base
 # get labels for all the aspects depends on their ideality
