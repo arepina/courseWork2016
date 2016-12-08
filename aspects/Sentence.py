80f31f971df04f38bf5b2c39f6a2d65bff402025
anastasia <anastasiya.repina2012@yandex.ru>
2016-12-08 18:58:33 +0300

    Partly filled db with sentences

--- aspects/Sentence.py
+++ aspects/Sentence.py
@@ -14,32 +14,33 @@ class Sentence:
         while row_aspect is not None:  # iterate through all reviews
             print(count)
             count += 1
-            article = str(row_aspect[2])
-            adv = str(row_aspect[3]).strip()
-            review = ""
-            if len(adv) != 0:
-                review += adv
-                if adv[len(adv) - 1] != "." and adv[len(adv) - 1] != "!" and adv[len(adv) - 1] != "?":
-                    review += "."
-            dis = str(row_aspect[4]).strip()
-            if len(dis) != 0:
-                if len(review) != 0:
-                    review += " "
-                review += dis
-                if dis[len(dis) - 1] != "." and dis[len(dis) - 1] != "!" and dis[len(dis) - 1] != "?":
-                    review += "."
-            com = str(row_aspect[5]).strip()
-            if len(com) != 0:
-                if len(review) != 0:
-                    review += " "
-                review += com
-                if com[len(com) - 1] != "." and com[len(com) - 1] != "!" and com[len(com) - 1] != "?":
-                    review += "."
-            sentences_from_api = self.ask_api(review.lower(), aspect)
-            sentences = self.clean_sentences(sentences_from_api)
-            for sentence in sentences:
-                if sentence != ".":
-                    db.add_sentence(article, sentence)
+            if count > 8284:
+                article = str(row_aspect[2])
+                adv = str(row_aspect[3]).strip()
+                review = ""
+                if len(adv) != 0:
+                    review += adv
+                    if adv[len(adv) - 1] != "." and adv[len(adv) - 1] != "!" and adv[len(adv) - 1] != "?":
+                        review += "."
+                dis = str(row_aspect[4]).strip()
+                if len(dis) != 0:
+                    if len(review) != 0:
+                        review += " "
+                    review += dis
+                    if dis[len(dis) - 1] != "." and dis[len(dis) - 1] != "!" and dis[len(dis) - 1] != "?":
+                        review += "."
+                com = str(row_aspect[5]).strip()
+                if len(com) != 0:
+                    if len(review) != 0:
+                        review += " "
+                    review += com
+                    if com[len(com) - 1] != "." and com[len(com) - 1] != "!" and com[len(com) - 1] != "?":
+                        review += "."
+                sentences_from_api = self.ask_api(review.lower(), aspect)
+                sentences = self.clean_sentences(sentences_from_api)
+                for sentence in sentences:
+                    if sentence != ".":
+                        db.add_sentence(article, sentence)
             row_aspect = db.cursor_reviews.fetchone()
 
     @staticmethod
