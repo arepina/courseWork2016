9508c6e37c84ee0332d3a729cee15f625fe72c2d
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-01-25 21:21:29 +0300

    Changed the method of calculation

--- aspects/Main.py
+++ aspects/Main.py
@@ -55,10 +55,10 @@ def pmi_process():
     pmi = PMI()
     reviews_corpus = pmi.get_all_reviews_corpus(db)  # reviews
     sentences_corpus = pmi.get_all_sentences_corpus(db)  # sentences
-    # pmi_ideal(pmi, reviews_corpus, sentences_corpus) # ideal
-    vocabulary = pmi.get_vocabulary(db)
+    pmi_ideal(pmi, reviews_corpus, sentences_corpus)  # ideal
+    # vocabulary = pmi.get_vocabulary(db)
     # pmi_review(pmi, reviews_corpus, vocabulary)
-    pmi_sentence(pmi, sentences_corpus, vocabulary)
+    # pmi_sentence(pmi, sentences_corpus, vocabulary)
 
 
 def pmi_review(pmi, reviews_corpus, vocabulary):
@@ -93,8 +93,8 @@ aspect = Aspects()
 # aspects_process()
 # sentence_process()
 # split_process()
-# pmi_process()
-semantic_learning_process()
+pmi_process()
+# semantic_learning_process()
 
 
 # Sizes data
